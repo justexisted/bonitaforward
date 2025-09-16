a1ab07ce5ef697c7331b634d556b015bee8efdb8
Chun K <justexisted@gmail.com>
2025-09-16 13:37:17 -0700

    fixing scrollfloat react import issue

--- src/components/ScrollFloat.tsx
+++ src/components/ScrollFloat.tsx
@@ -1,4 +1,5 @@
-import type { useEffect, useMemo, useRef, ReactNode, RefObject } from 'react';
+import type { ReactNode, RefObject } from 'react';
+import React, { useEffect, useMemo, useRef } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
